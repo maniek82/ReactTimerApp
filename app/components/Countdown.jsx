373440b379f4a02411a22db18c706e432f118135
maniek82 <mariusz.tripler@gmail.com>
2016-10-23 09:01:28 +0100

    craete Clock component

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,10 +1,11 @@
 var React = require('react');
+var Clock = require('Clock');
 
 var Countdown = React.createClass ({
 	render: function () {
 		return (
-			<p>Countdown.jsx</p>
-			)
+			<Clock totalSeconds={129} />
+			);
 	}
 });
 
